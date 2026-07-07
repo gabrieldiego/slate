4d6c0bba4288a94b6a7e836a5fe891be6e97ec01
Gabriel Diego Teixeira <gabrieldiegoteixeira@zoho.com>
2026-07-07 04:00:26 -0700

    Use local fork dependency sources

--- scripts/fetch-deps.sh
+++ scripts/fetch-deps.sh
@@ -6,3 +6,9 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 source "${ROOT}/scripts/local-env.sh"
 
 ns-clone -d -s "$@"
+
+if [ -d "${TARGET_WORKSPACE}/duktape/.git" ]; then
+	git -C "${TARGET_WORKSPACE}/duktape" remote set-url origin "${DUKTAPE_REPO_URI}"
+else
+	git clone "${DUKTAPE_REPO_URI}" "${TARGET_WORKSPACE}/duktape"
+fi
--- scripts/local-env.sh
+++ scripts/local-env.sh
@@ -4,7 +4,8 @@ set -eo pipefail
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 export TARGET_WORKSPACE="${TARGET_WORKSPACE:-${ROOT}/projects}"
-export REPO_BASE_URI="${REPO_BASE_URI:-https://github.com/netsurf-browser}"
+export REPO_BASE_URI="${REPO_BASE_URI:-https://github.com/gabrieldiego}"
+export DUKTAPE_REPO_URI="${DUKTAPE_REPO_URI:-https://github.com/gabrieldiego/duktape}"
 export TARGET_TOOLKIT="${TARGET_TOOLKIT:-gtk3}"
 
 # docs/env.sh is intended to be sourced. It defines PREFIX, BUILD_PREFIX,
